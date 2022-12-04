d9dc135950af5096682389522ff8c89d5f5552d5
R.J. Robinson <robertj.robinson@gmail.com>
2022-12-04 15:36:35 -0500

    updated main java file

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,14 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"));
-//         fixme: change this to a proper title later
-        primaryStage.setTitle("WGU | SW2 | C195");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/users/login_page.fxml")));
+
+            primaryStage.setTitle("WGU | SW2 | C195");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
